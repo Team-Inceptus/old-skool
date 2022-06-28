a66d18fbebb5a7444f26feafbc4879a52d82af7f
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-06-28 18:10:18 -0400

    feat: in 32-bit protected mode!

--- src/stage1.asm
+++ src/stage1.asm
@@ -28,6 +28,17 @@ _start:
     call print
 
     cli
+
+    ;; Load our GDT into GDT register.
+    lgdt [gdt_desc]
+
+    ;; Set protected mode bit.
+    mov eax, cr0
+    or eax, 1 << 0
+    mov cr0, eax
+
+    jmp CODE_SEG:protected_mode
+
     hlt
 
 
@@ -83,6 +94,74 @@ dap:
 boot_msg: db "Booting..", NEWLINE, NEWLINE, 0
 disk_loaded_msg: db 0xD, "Loaded stage2 from disk!", NEWLINE, 0
 disk_err_msg: db "FATAL: Failed to load from disk!", 0
+
+gdt_start:
+    gdt_null:
+        dd 0x0
+        dd 0x0
+    gdt_code:
+        ;; Type flags:
+        ;; Present: 1 since we are using code.
+        ;; Privilege: 00 higest privilige.
+        ;; Descriptor type: 1 for code/data.
+        ;; Code: 1.
+        ;; Conforming: 0 so segments with a lower privilege may not call code in this segment.
+        ;; Readable: 1.
+        ;; Accessed: 0.
+
+        ;; Other flags:
+        ;; Granularity: 1 so we can reach father into memory.
+        ;; 32-bit default: 1 since our segment will have 32-bit code.
+        ;; 64-bit code segment: 0.
+        ;; AVL 0.
+        ;; Limit: 1111.
+
+        dw 0xFFFF        ;; Limit.
+        dw 0x0           ;; Base.
+        db 0x0           ;; Base.
+        db 0b10011010    ;; 1st flags, type flags.
+        db 0b11001111    ;; 2nd flags, type flags.
+        db 0x0
+    gdt_data:
+        ;; Type flags:
+        ;; Code: 0.
+        ;; Expand down: 0.
+        ;; Writable: 0.
+        ;; Accessed: 0.
+
+        dw 0xFFFF        ;; Limit.
+        dw 0x0           ;; Base.
+        db 0x0           ;; Base.
+        db 0b10010010    ;; 1st flags, type flags.
+        db 0b11001111    ;; 2nd flags, type flags.
+        db 0x0
+gdt_end:
+
+
+gdt_desc:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+bits 32
+protected_mode:
+    ;; Set segment registers.
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ;; Update stack so it points to top
+    ;; of our free space.
+    mov ebp, 0x90000
+    mov esp, ebp                ;; Point top of stack to base.
+
+    cli
+    hlt
 
 ;; Some sector padding.
 times 510-($-$$) db 0
